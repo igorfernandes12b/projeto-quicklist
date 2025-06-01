26c7d74fae00156de01a982d52b399541e905a57
igorfernandesrodrigues <igor.fernandes.jogos@gmail.com>
2025-05-31 21:41:18 -0300

    21 commit

--- main.js
+++ main.js
@@ -1,16 +1,42 @@
 const items = []
 
-function addItem (){
+function addItem() {
     const itemName = document.querySelector("#item").value
 
     const item = {
         name: itemName,
         checked: false
-
     }
 
-
     items.push(item)
+    
 
     document.querySelector("#item").value = ""
+
+    showItemsList()
+}
+
+function showItemsList() {
+    const sectionList = document.querySelector(".list")
+    sectionList.textContent = ""
+    
+    items.map((item, index) => {
+        sectionList.innerHTML += `
+            <div class="item">
+                <div>
+                    <input type="checkbox" name="List" id="item-${index}">
+
+                    <div class="custom-checkbox">
+                        <img src="./assets/checked.svg" alt="checked">
+                    </div>
+
+                    <label for="item-${index}">${item.name}</label>
+                </div>
+
+                <button>
+                    <img src="./assets/trash-icon.svg" alt="trash icon">
+                </button>
+            </div>
+        `
+    })
 }
