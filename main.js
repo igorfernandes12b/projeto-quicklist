e730bbdd936f9939b1ae1873c4605b0016d34ca0
igorfernandesrodrigues <igor.fernandes.jogos@gmail.com>
2025-05-31 21:59:41 -0300

    24 commit

--- main.js
+++ main.js
@@ -32,7 +32,6 @@ function showItemsList() {
                     <label for="item-${index}">${item.name}</label>
                 </div>
 
-                <button>
                 <button onclick="removeItem('${item.name}')">
                     <img src="./assets/trash-icon.svg" alt="trash icon">
                 </button>
@@ -41,9 +40,15 @@ function showItemsList() {
     })
 }
 
-
 function removeItem(itemName) {
     const itemIndex = items.findIndex(item => item.name === itemName)
+    const divWarning = document.querySelector(".warning")
+
+    divWarning.classList.remove("hide-warning")
+
+    setTimeout(() => {
+        divWarning.classList.add("hide-warning")
+    }, 4000)
 
     if (itemIndex !== -1) {
         items.splice(itemIndex, 1)
